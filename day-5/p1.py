90987f94c9881881b79277e306eb1b7d0d96e1c3
psapir <psapir@aligntech.com>
2024-06-24 15:24:28 +0200

    Finished part 1

--- day-5/p1.py
+++ day-5/p1.py
@@ -5,6 +5,20 @@ import re
 num_regex = r"(\d+)"
 
 
+def apply_map(input, map):
+    for dest_start, src_start, rng_len in map:
+        if input in range(src_start, src_start+rng_len):
+            return dest_start + input - src_start
+    return input
+
+
+def apply_all_maps(seed):
+    output = seed
+    for map in maps.values():
+        output = apply_map(output, map)
+    return output
+
+
 if __name__ == "__main__":
     with open(argv[1], "r") as f:
         data = [
@@ -20,4 +34,7 @@ if __name__ == "__main__":
         ]
         for i in range(1, 8)
     }
-    # print(maps)
+
+    outputs = [ apply_all_maps(seed) for seed in seeds ]
+    print("----------")
+    print(min(outputs))
