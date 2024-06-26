d4e98740e8e6deb5aaf375e3272011ec33248688
psapir <psapir@aligntech.com>
2024-06-26 15:26:52 +0200

    Reversed search

--- day-5/p2.py
+++ day-5/p2.py
@@ -1,6 +1,7 @@
 from sys import argv
 import re
 from tqdm import tqdm
+from copy import deepcopy
 
 
 num_regex = r"(\d+)"
@@ -13,7 +14,7 @@ def apply_map(input, map):
     return input
 
 
-def apply_all_maps(seed):
+def apply_all_maps(seed, maps):
     output = seed
     for map in maps.values():
         output = apply_map(output, map)
@@ -40,9 +41,9 @@ if __name__ == "__main__":
         ]
     seeds_raw = [ int(x) for x in re.findall(num_regex, data[0][0]) ]
     seed_ranges = [
-        range(start, start+end)
-        for start, end in zip(seeds_raw[::2], seeds_raw[1::2])
+        range(first, last) for first, last in zip(seeds_raw[::2], seeds_raw[1::2])
     ]
+
     maps = {
         data[i][0].replace(" map:", ""): [
             [ int(y) for y in x.split() ]
@@ -50,12 +51,26 @@ if __name__ == "__main__":
         ]
         for i in range(1, 8)
     }
-    
-    with open("minimums.txt", "w") as f:
-        for i, rng in enumerate(tqdm(seed_ranges)):
-            min = -1
-            for seed in tqdm(rng, leave=False):
-                output = apply_all_maps(seed)
-                if output < min or min == -1:
-                    min = output
-            f.write(f"Part {i}: {min}\n")
+
+    inv_maps = deepcopy(maps)
+    for map in inv_maps.values():
+        for rng in map:
+            rng[:2] = reversed(rng[:2])
+    inv_maps = {
+        key: inv_maps[key]
+        for key in reversed(inv_maps)
+    }
+
+    # for seed in seeds_raw:
+    #     output = apply_all_maps(seed, maps)
+    #     seed_back = apply_all_maps(output, inv_maps)
+    #     print(f"{seed} --> {output} --> {seed_back}")
+
+    sorted_location_ranges = sorted(maps["humidity-to-location"])
+    for location_range in tqdm(sorted_location_ranges):
+        dest, _, num = location_range
+        for location in tqdm(range(dest, dest+num), leave=False):
+            seed = apply_all_maps(location, inv_maps)
+            if seed in seed_ranges:
+                print(f"Seed: {seed} --> location: {location}")
+                exit(0)
